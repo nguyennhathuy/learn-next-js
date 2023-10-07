4adf7fabdb1cdd18cd4946d81a190a3deeecc1eb
Huy Nguyen <nhathuy120796@gmail.com>
2023-10-07 23:55:28 +0700

    daily commit 4

---
+++ utils/enum/index.js
@@ -0,0 +1,22 @@
+export const clients = [
+    {
+      clientId: 'Huy',
+      fullName: 'Nguyen Nhat Huy'
+    },
+    {
+      clientId: 'Khuong',
+      fullName: 'Nguyen Nhat Khuong'
+    },
+    {
+      clientId: 'Duy',
+      fullName: 'Nguyen Nhat Duy'
+    },
+  ];
+
+export const COLOR = [
+    'Black', 'White', 'Blue'
+]
+
+export const SIZE = [
+    'XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'
+]
